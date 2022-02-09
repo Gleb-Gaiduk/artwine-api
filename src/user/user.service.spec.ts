de3d06acb7901851cae5602ee7bb9e7af6a48acd
Hleb Haiduk <glebgaiduk.pl@gmail.com>
2022-02-09 15:43:03 +0100

    Add test case

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { MockType } from '../../test/types';
+import { PaginateService } from '../utils/paginate/paginate.service';
+import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
 
+  const userRepoMock: MockType<Repository<User>> = {
+    save: jest.fn(),
+    findOne: jest.fn(),
+    find: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        PaginateService,
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: userRepoMock,
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
